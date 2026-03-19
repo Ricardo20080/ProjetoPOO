cc776ea5a3428cbd4249cbd327554bf8fa2d5b98
maxxvinycius <maxvinycius36@gmail.com>
2026-03-19 00:51:53 -0300

    Add files via upload

---
+++ RPG/src/rpg/Guerreiro.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpg;
+
+/**
+ *
+ * @author maxvi
+ */
+public class Guerreiro extends Personagem {
+    public Guerreiro(String nome) {
+        super(nome, 120, 15);
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println(getNome() + " ataca com espada!");
+    }
+}
+
